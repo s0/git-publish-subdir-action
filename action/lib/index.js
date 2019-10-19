bd6b981b06c7f343bd0ae243fac8cab7174de1cf
Sam Lanning <sam@lanni.ng>
2019-10-18 21:38:14 -0700

    Started config parsing

--- action/lib/index.js
+++ action/lib/index.js
@@ -1,1 +1,50 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var git_url_parse_1 = __importDefault(require("git-url-parse"));
+// Environment Variables
+/**
+ * The URL of the repository to push to, one-of:
+ *
+ * * an ssh URL to a repository
+ */
+var REPO = process.env.REPO;
+/**
+ * The name of the branch to push to
+ */
+var BRANCH = process.env.BRANCH;
+/**
+ * Which subdirectory in the repository to we want to push as the contents of the branch
+ */
+var FOLDER = process.env.FOLDER;
+/**
+ * The private key to use for publishing if REPO is an SSH repo
+ */
+var SSH_PRIVATE_KEY = process.env.SSH_PRIVATE_KEY;
+var config = (function () {
+    if (!BRANCH)
+        throw new Error('BRANCH must be specified');
+    var branch = BRANCH;
+    if (!FOLDER)
+        throw new Error('FOLDER must be specified');
+    var folder = FOLDER;
+    if (!REPO)
+        throw new Error('REPO must be specified');
+    // Determine the type of URL
+    var url = git_url_parse_1.default(REPO);
+    if (url.protocol === 'ssh') {
+        if (!SSH_PRIVATE_KEY)
+            throw new Error('SSH_PRIVATE_KEY must be specified when REPO uses ssh');
+        var config_1 = {
+            branch: branch,
+            folder: folder,
+            mode: 'ssh',
+            url: REPO,
+            privateKey: SSH_PRIVATE_KEY
+        };
+        return config_1;
+    }
+    throw new Error('Unsupported REPO URL');
+})();
--- action/src/index.ts
+++ action/src/index.ts
@@ -1,1 +1,63 @@
-console.log ("foo");
+import gitUrlParse from "git-url-parse";
+
+// Environment Variables
+
+/**
+ * The URL of the repository to push to, one-of:
+ * 
+ * * an ssh URL to a repository
+ */
+const REPO = process.env.REPO;
+/**
+ * The name of the branch to push to
+ */
+const BRANCH = process.env.BRANCH;
+/**
+ * Which subdirectory in the repository to we want to push as the contents of the branch
+ */
+const FOLDER = process.env.FOLDER;
+/**
+ * The private key to use for publishing if REPO is an SSH repo
+ */
+const SSH_PRIVATE_KEY = process.env.SSH_PRIVATE_KEY;
+
+interface BaseConfig {
+  branch: string;
+  folder: string;
+}
+
+interface SshConfig extends BaseConfig {
+  mode: 'ssh';
+  url: string;
+  privateKey: string;
+}
+
+type Config = SshConfig;
+
+const config: Config = (() => {
+  if (!REPO)
+    throw new Error('REPO must be specified');
+  if (!BRANCH)
+    throw new Error('BRANCH must be specified');
+  if (!FOLDER)
+    throw new Error('FOLDER must be specified');
+
+  // Determine the type of URL
+  const branch = BRANCH;
+  const folder = FOLDER;
+  const url = gitUrlParse(REPO);
+
+  if (url.protocol === 'ssh') {
+    if (!SSH_PRIVATE_KEY)
+      throw new Error('SSH_PRIVATE_KEY must be specified when REPO uses ssh');
+    const config: Config = {
+      branch,
+      folder,
+      mode: 'ssh',
+      url: REPO,
+      privateKey: SSH_PRIVATE_KEY
+    }
+    return config;
+  }
+  throw new Error('Unsupported REPO URL');
+})();
