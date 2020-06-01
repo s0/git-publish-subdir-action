4e2d370d00f44848bce9ebd790f2dc742eed936f
s0 <s0@users.noreply.github.com>
2020-06-01 19:03:26 +0100

    Update Typescript

--- action/test/jest.config.js
+++ action/test/jest.config.js
@@ -2,11 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: '../',
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*'
-  ],
-  collectCoverage: true,
-  "coverageReporters": ["none"],
   globalSetup: '<rootDir>/test/jest-global-setup.ts',
   globalTeardown: '<rootDir>/test/jest-global-teardown.ts',
 };
